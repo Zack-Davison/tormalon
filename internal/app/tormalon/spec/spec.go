214957d937b66194558047d8926d6561534841a8
Zack Davison <zackdavison@Zacks-MacBook-Air.local>
2026-09-10 22:05:53 +0100

    Implemented initial struct in spec

---
+++ internal/app/tormalon/spec/spec.go
@@ -0,0 +1,11 @@
+package spec
+
+//Handle the parsing of data from the cli into a format that can be passed into a template renderer
+
+type Project struct {
+	projectOwner string
+	projectName  string
+	private      bool
+}
+
+//Need to investigate data requirements further to see what needs to be implemented
